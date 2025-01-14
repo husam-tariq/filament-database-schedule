4fcff673391e26b1a63fea6b0172250bf8adc8d5
dorgan@donaldorgan.com <dorgan@donaldorgan.com>
2025-01-14 12:00:40 -0500

    Fix for #44

--- src/Http/Services/ScheduleHistoryService.php
+++ src/Http/Services/ScheduleHistoryService.php
@@ -6,7 +6,7 @@ namespace HusamTariq\FilamentDatabaseSchedule\Http\Services;
 class ScheduleHistoryService
 {
     public static function prune($schedule) {
-        $keepIds = $schedule->histories()->select('id')->latest()->take($schedule->max_history_count);
+        $keepIds = $schedule->histories()->select('id')->latest()->take($schedule->max_history_count)->pluck('id')->toArray();
         $schedule->histories()->whereNotIn('id', $keepIds)->delete();
     }
 }
